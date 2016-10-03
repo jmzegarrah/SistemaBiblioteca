281fcd6b54bc4c359dd3a32da3098b20b1d2a320
Ark4n0s <jmzegarrah@gmail.com>
2016-10-03 16:58:22 -0500

    Primer Commit

---
+++ db.js
@@ -0,0 +1,5 @@
+module.exports = {
+	/*'url' : 'mongodb://<dbuser>:<dbpassword>@novus.modulusmongo.net:27017/<dbName>'
+	/*'url' : 'mongodb://root:soloporti29@ds021326.mlab.com:21326/bibliotecaunsa' */
+	'url' : 'mongodb://localhost/biblioteca'
+}

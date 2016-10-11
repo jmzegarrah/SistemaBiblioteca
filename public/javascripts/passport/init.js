ce78d682a1892fd4e59eca8208e0879f77fdcb83
Ark4n0s <jmzegarrah@gmail.com>
2016-10-10 22:00:41 -0500

    Reestructurando el proyecto

--- public/javascripts/passport/init.js
+++ public/javascripts/passport/init.js
@@ -1,6 +1,6 @@
 var login = require('./login');
 var signup = require('./signup');
-var User = require('../models/user');
+var User = require('../../../app_server/models/user');
 
 module.exports = function(passport){
 
